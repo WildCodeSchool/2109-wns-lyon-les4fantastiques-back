da02f59616dad0fa325923a76ca4efc776edfb34
RangoDisco <rango1@posteo.net>
2021-11-23 14:38:11 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
-import { WildersResolver } from './resolvers/Wilders';
-import { buildSchema } from 'type-graphql';
-import { createConnection } from 'typeorm';
+import { WildersResolver } from "./resolvers/Wilders";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
-    // database connection, the config is loaded from ormconfig.json
-    await createConnection();
+  // database connection, the config is loaded from ormconfig.json
+  await createConnection();
 
-    // ... Building schema here
-    const schema = await buildSchema({
-        resolvers: [WildersResolver],
-    });
+  // ... Building schema here
+  const schema = await buildSchema({
+    resolvers: [WildersResolver],
+  });
 
-    // Create the GraphQL server
-    const server = new ApolloServer({
-        schema,
-    });
+  // Create the GraphQL server
+  const server = new ApolloServer({
+    schema,
+  });
 
-    // Start the server
-    const { url } = await server.listen(PORT);
-    console.log(`Server is running, GraphQL Playground available at ${url}`);
+  // Start the server
+  const { url } = await server.listen(PORT);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
 
 bootstrap();
