b4678a988e23b6eafe5bc79e527b950e652c3e7a
RangoDisco <rango1@posteo.net>
2021-11-24 16:52:55 +0100

    typed verify password params

--- src/helpers/auth/password.ts
+++ src/helpers/auth/password.ts
@@ -3,6 +3,6 @@ import * as argon2 from "argon2";
 export const hashPassword = (plainPassword: string) => {
   return argon2.hash(plainPassword);
 };
-export const verifyPassword = (plainPassword, hashedPassword) => {
+export const verifyPassword = (plainPassword: string, hashedPassword: string) => {
   return argon2.verify(hashedPassword, plainPassword);
 };
